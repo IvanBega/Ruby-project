eb3b50ac5e4215a3c022be3c021736a1582a1c7d
Ivan Bega <bega.ivan03@gmail.com>
2022-11-21 00:55:19 +0200

    crus

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :students
   #get 'home/index'
+  get 'home/dormitory'
+  get 'home/teachers'
+  get 'home/pedagogical'
+  get 'home/groups'
   root 'home#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
